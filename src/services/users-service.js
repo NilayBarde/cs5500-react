d122b1ba93a50ce046f1b4f18632a181e1e085ca
Nilay Barde <nilaybarde@gmail.com>
2022-03-12 13:36:04 -0500

    Update base urls

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-const BASE_URL = "https://a2-cs5500.herokuapp.com/api";
-// const BASE_URL = "http://localhost:4000/api";
 
-const LOGIN_API = `${BASE_URL}/login`;
-const USERS_API = `${BASE_URL}/users`;
+const LOGIN_API = "https://a2-cs5500.herokuapp.com/api/login";
+const USERS_API = "https://a2-cs5500.herokuapp.com/api/users";
 
 export const createUser = (user) =>
     axios.post(`${USERS_API}`, user).then((response) => response.data);
