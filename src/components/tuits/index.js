b1f94f0ba820039f1015c9b342780191043a4312
Nilay Barde <nilaybarde@gmail.com>
2022-03-13 21:18:52 -0400

    update package.json

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,21 +1,24 @@
 import React from "react";
-import './tuits.css';
+import "./tuits.css";
 import Tuit from "./tuit";
 
-function Tuits({tuits = [], deleteTuit}) {
+export const Tuits = ({ tuits = [], deleteTuit }) => {
     return (
-    <div>
-      <ul className="ttr-tuits list-group">
-        {
-          tuits.map && tuits.map(tuit => {
-            return (
-              <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
-            );
-          })
-        }
-      </ul>
-    </div>
-  );
-}
+        <div>
+            <ul className="ttr-tuits list-group">
+                {tuits.map &&
+                    tuits.map((tuit) => {
+                        return (
+                            <Tuit
+                                key={tuit._id}
+                                deleteTuit={deleteTuit}
+                                tuit={tuit}
+                            />
+                        );
+                    })}
+            </ul>
+        </div>
+    );
+};
 
 export default Tuits;
